400916f8494b1e51a97d1b9e2fbf13d9bd3d98e3
AndreaRispoli97 <Andrea9715@outlook.it>
2025-03-03 15:50:05 +0100

    add: seconda soluzione

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,31 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+// function getFirstLetter(names) {
+//     return names.map(name => name[0]);
+// }
+
+// console.log(getFirstLetter(names));
+
+function getFirstLetter(names) {
+    const namesInitials = [];
+    for (let i = 0; i < names.length; i++) {
+        namesInitials.push(names[i][0]);
+    }
+    return namesInitials;
+}
+
+// const getFirstLetter = names => {
+//     const namesInitials = [];
+//     for (let i = 0; i < names.length; i++) {
+//         namesInitials.push(names[i][0]);
+//     }
+//     return namesInitials;
+// }
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(getFirstLetter(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
