28bac9a777eb6b3446f879dde712d7b36d5f730e
AndreaRispoli97 <Andrea9715@outlook.it>
2025-03-03 18:26:38 +0100

    add: quinta soluzione

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -10,9 +10,32 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+function goodMorning(name, time) {
+    if (time <= 13) {
+        return `Buongiorno ${name}.`;
+    } else if (time <= 17) {
+        return `Buon pomeriggio ${name}.`;
+    } else {
+        return `Buonasera ${name}.`;
+    }
+}
+
+
+// const goodMorning = (name, time) => {
+//     if (time <= 13) {
+//         return `Buongiorno ${name}.`;
+//     } else if (time <= 17) {
+//         return `Buon pomeriggio ${name}.`;
+//     } else {
+//         return `Buonasera ${name}.`;
+//     }
+// }
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(goodMorning(name, 10));
+console.log(goodMorning(name, 15));
+console.log(goodMorning(name, 18));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
